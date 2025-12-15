ddfccfbb09c21196a36932f9167124def4f813b8
teves <antony2020teves@gmail.com>
2025-12-15 17:00:44 -0500

    guardando react

---
+++ semana12/ecomerce/src/App.jsx
@@ -0,0 +1,14 @@
+import ListProductview from "./view/ListProductview";
+import Navegation from "./components/Navegation";
+
+const App = () => {
+  return (
+    <> 
+      <Navegation/> 
+    <ListProductView/>
+
+    </>
+  );
+};
+
+export default App;
---
+++ semana12/ecomerce/src/components/Navegation.jsx
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Navegation = () =>{
+    return (
+<nabvar className="bg-body-tertiary">
+  <Container>
+    <Navbar.Brand> Admin </Navbar.Brand>
+    </Container>
+    </nabvar>
+    )};
+export default Navegation;
